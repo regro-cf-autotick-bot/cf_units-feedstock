54adc1a8057a2e06437c77c3600bf670b4c67ce0
Filipe Fernandes <ocefpaf@gmail.com>
2018-05-21 13:37:36 -0300

    update to v2.0.1

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure cf_units can find the udunits library.
-SITECFG=cf_units/etc/site.cfg
-echo "[System]" > $SITECFG
-echo "udunits2_path = $PREFIX/lib/libudunits2${SHLIB_EXT}" >> $SITECFG
-echo "udunits2_xml_path = $PREFIX/share/udunits/udunits2.xml" >> $SITECFG
-
-$PYTHON setup.py install --single-version-externally-managed  --record record.txt
