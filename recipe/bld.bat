2e6ef3afea7321bc760f5239eb0ae36ae5cb1ce2
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-24 09:45:44 -0300

    minor fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,5 @@ set SITECFG=cf_units/etc/site.cfg
 echo [System] > %SITECFG%
 echo udunits2_xml_path = %LIBRARY_PREFIX%\share\udunits\udunits2.xml >> %SITECFG%
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed .
+"%PYTHON%" -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ SITECFG=cf_units/etc/site.cfg
 echo "[System]" > $SITECFG
 echo "udunits2_xml_path = $PREFIX/share/udunits/udunits2.xml" >> $SITECFG
 
-$PYTHON -m pip install --no-deps --ignore-installed .
+$PYTHON -m pip install . --no-deps -vv
